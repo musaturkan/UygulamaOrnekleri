989d954cd47c26a8a2b49899ff02d565dac4d0ea
Musa Turkan <musa.turkan@bilecik.edu.tr>
2026-03-13 12:09:57 +0300

    ConnnectionString bilgisi ayar dosyasına taşındı.

--- DataModel/LokantaContext.cs
+++ DataModel/LokantaContext.cs
@@ -22,10 +22,14 @@ namespace DataModel
         ///Veri tabanı bağlantı işlemleri için OnConfiguring metodu kullanılır. Bu metod içinde UseSqlServer gibi bir yöntemle veritabanı bağlantı dizesi sağlanır.
         ///
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {    
-            //string connectionString = ConfigurationManager.ConnectionStrings["LokantaConnection"].ConnectionString;
-            optionsBuilder.UseSqlServer(@"Server=LAB708-PC;Database=Lokanta;Trusted_Connection=True;TrustServerCertificate=True");
-            //optionsBuilder.UseSqlServer(connectionString);
+        {
+            ///ConnectionString bilgisini ayar yönetiminden alarak kullanmak, uygulamanın farklı ortamlarda (geliştirme, test, üretim) çalışırken farklı veritabanlarına bağlanmasını kolaylaştırır. Bu sayede kodun içinde bağlantı dizesi hard-coded olarak bulunmaz ve güvenlik açısından da daha iyi bir yaklaşım olur.
+            string? connectionString = AyarYonetim.DbAyar.GetConnectionString("LokantaConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+
+            ///ConnectionString bilgisini doğrudan kod içinde tanımlamak, hızlı bir şekilde veritabanına bağlanmak için kullanılabilir, ancak bu yöntem genellikle önerilmez çünkü bağlantı dizesi kod içinde hard-coded olarak bulunur ve güvenlik riskleri oluşturabilir. Ayrıca, farklı ortamlarda farklı veritabanlarına bağlanmak gerektiğinde esneklik sağlamaz.
+            ///optionsBuilder.UseSqlServer(@"Server=LAB708-PC;Database=Lokanta;Trusted_Connection=True;TrustServerCertificate=True");
+         
         }
 
 
